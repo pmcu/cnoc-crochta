3f2dbbebc392bf5c34aa9137344c3e91acc26019
pmcu <pmcumm@gmail.com>
2018-12-28 12:42:18 +0000

    test php

---
+++ footer.php
@@ -0,0 +1,3 @@
+<?php
+echo "<p>PMCU Geafta Dubh; .date("y") </p>";
+?>
---
+++ test.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<body>
+
+<h1>Fáilte</h1>
+<p>Téacs le dul isteach</p>
+<p>Some more text.</p>
+<?php include 'footer.php';?>
+
+</body>
+</html>
