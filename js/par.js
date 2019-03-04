917e50554c5b167927d548efc27fe937c85a55d7
pmcu <pmcumm@gmail.com>
2019-03-04 09:41:20 +0000

    nuashonrú 03-03-19

--- js/par.js
+++ js/par.js
@@ -1,20 +1,29 @@
 
-var par2 ="<h2>An Scéal</h2>Seo an dara halt. \
-Boc mór buí a bhí ann. \
-Chaitheadh sé hata mór biorach.";
+var par2 ="<h2>The Old Time Entries</h2>Seo an dara halt. \
+B'fhéidir nach bhfuil a mhalairt de théarma ann don ghné sráide ach\
+ <i>bealach isteach<i>."
+ var sonra1 = "<table>\
+ <th>Rud</th><th>Rud</th><th>Rud</th>\
+ <tr><td>aon</td><td>dó</td><td>trí</td></tr>\
+ </table>"
+var par3 ="<p><blockquote>\
+There was different entries. I fairly enjoyed goin' up them myself. \
+<br><cite>Long Peadar, of Lower Newholland</cite></blockquote></p>"
 function myFunction() {
-  document.getElementById("demo").innerHTML = "<h2>Up the Entry</h2><p>Sin mar dhéanfadh\
-  tagairt do áit san am. Bhí trí ghnó thuas ansin. Bheadh sé chomh maith\
-  againn cur síos orthu ó cheann go ceann. \
-  <p>agus an lá sin \
-  bhí sé deas agus buí.</p>";
+  document.getElementById("demo").innerHTML = par3;
   document.getElementById('myImage').src='pic/blessed.jpg';
   document.getElementById("demo2").innerHTML=par2;
-  var audio = new Audio( "ceol/budda.mp3" ); audio.play();
+  //var audio = new Audio( "ceol/budda.mp3" ); audio.play();
 }
 function myFunction2() {
 
 document.getElementById("demo").innerHTML = "<h2>Bye byes, you boy you.</h2>";
 document.getElementById('myImage').src='pic/fear.jpg';
 document.getElementById("demo2").innerHTML="";
+}
+function myFunction3() {
+document.getElementById("eol").innerHTML = sonra1;
+document.getElementById("demo").innerHTML="";
+document.getElementById("demo2").innerHTML="";
+document.getElementById('myImage').src='pic/swanie.jpg';
 }
