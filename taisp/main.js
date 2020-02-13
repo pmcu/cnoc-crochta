9a1acadb7a83030d34d83a8dd712f39f8354f98b
pmcu <pmcumm@gmail.com>
2020-02-13 17:51:42 +0000

    ns taisp

--- taisp/main.js
+++ taisp/main.js
@@ -16,7 +16,7 @@ images[8] = 'img/5.jpg';
 images[9] = 'img/6.jpg';
 images[10] = 'img/32.jpg';
 images[11] = 'img/11b.jpg';
-images[12] = 'img/7.jpg';
+images[12] = 'img/53.jpg';
 images[13] = 'img/41.png';
 images[14] = 'img/8.jpg';
 images[15] = 'img/21.jpg';
@@ -24,7 +24,7 @@ images[16] = 'img/19.jpg';
 images[17] = 'img/14.jpg';
 images[18] = 'img/49.jpg';
 images[19] = 'img/22.jpg';
-images[20] = 'img/4.jpg';
+images[20] = 'img/15.jpg';
 images[21] = 'img/27.jpg';
 images[22] = 'img/46.jpg';
 images[23] = 'img/38.jpg';
@@ -32,7 +32,29 @@ images[24] = 'img/25.jpg';
 images[25] = 'img/29.jpg';
 images[26] = 'img/45.jpg';
 images[27] = 'img/39.jpg';
-images[28] = 'img/2.jpg';
+images[28] = 'img/83.jpg';
+images[29] = 'img/60.jpg';
+images[30] = 'img/78.jpg';
+images[31] = 'img/83.jpg';
+images[32] = 'img/76.jpg';
+images[33] = 'img/48.jpg';
+images[34] = 'img/68.jpg';
+images[35] = 'img/54.jpg';
+images[36] = 'img/55.jpg';
+images[37] = 'img/50.jpg';
+images[38] = 'img/61.jpg';
+images[39] = 'img/24.jpg';
+images[40] = 'img/51.jpg';
+images[41] = 'img/82.jpg';
+images[42] = 'img/64.png';
+images[43] = 'img/23.jpg';
+images[44] = 'img/57.jpg';
+images[45] = 'img/57.jpg';
+images[46] = 'img/80.jpg';
+images[47] = 'img/73.jpg';
+images[48] = 'img/64.png';
+images[49] = 'img/75.JPG';
+images[49] = 'img/63.png';
 
 // change image
 function changeImg(){
@@ -48,4 +70,3 @@ setTimeout("changeImg()", time);
 }
 
 window.onload = changeImg;
-
